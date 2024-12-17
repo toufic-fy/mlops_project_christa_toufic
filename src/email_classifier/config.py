d5982a2fdc27ba46f75fc2f2bde3166a92300ba1
toufic-fy <160768965+toufic-fy@users.noreply.github.com>
2024-12-17 18:33:04 +0200

    Feature/adding-config-and-data-loader (#3)
    * Add dataset, src structure, and config files
    
    * Add .gitignore file
    
    * Add pyproject.toml and poetry.lock files
    
    * Removing data from repository
    
    * Update README and config.yaml; add config.py for project configuration
    
    * Add data loader module with base_loader, csv_loader, json_loader, and factory classes
    
    ---------
    
    Co-authored-by: christamatta <christa.matta17@gmail.com>
    Co-authored-by: toufic.f <t.fakhry@ob-soft.com>

---
+++ src/email_classifier/config.py
@@ -0,0 +1,21 @@
+from omegaconf import OmegaConf
+from pydantic import BaseModel
+
+# Define basic Pydantic models for validation
+class ProjectConfig(BaseModel):
+    name: str
+    version: str
+
+class PathsConfig(BaseModel):
+    raw_data: str
+
+class Config(BaseModel):
+    project: ProjectConfig
+    paths: PathsConfig
+
+# Function to load the config.yaml file
+def load_config(config_path: str = "config.yaml") -> Config:
+    # Load the YAML file with OmegaConf
+    raw_config = OmegaConf.load(config_path)
+    # Convert the OmegaConf object to a dictionary and validate with Pydantic
+    return Config(**OmegaConf.to_container(raw_config, resolve=True))
