0dfabace6fccc85899b0ab3609abde8db27b7e38
toufic.f <t.fakhry@ob-soft.com>
2024-12-18 19:25:18 +0200

    fixing vectorize return type

--- src/email_classifier/vectorizer/tfidf_vectorizer.py
+++ src/email_classifier/vectorizer/tfidf_vectorizer.py
@@ -3,5 +3,5 @@ from .base_text_vectorizer import BaseTextVectorizer
 from typing import List
 
 class TfidfVectorizer(BaseTextVectorizer):
-    def vectorize(self, documents: List[str]) -> list:
+    def vectorize(self, documents: List[str]) -> List[str]:
         return self.vectorizer.fit_transform(documents)
