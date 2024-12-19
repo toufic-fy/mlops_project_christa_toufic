116c20dcb9a9e4ea4661b8a14fda8976875f1c35
toufic-fy <160768965+toufic-fy@users.noreply.github.com>
2024-12-19 12:18:53 +0200

    Feature/adding vectorizer class (#10)
    * completing vectorizer implementation
    
    ---------
    
    Co-authored-by: toufic.f <t.fakhry@ob-soft.com>

--- src/email_classifier/vectorizer/factory.py
+++ src/email_classifier/vectorizer/factory.py
@@ -3,16 +3,15 @@ from typing import Union, Any
 
 class VectorizerFactory:
     """Factory class to create vectorizers based on configuration."""
-    
+    vectorizers = {
+        "tfidf": TfidfVectorizer,
+        "bow": CountVectorizer,
+    }
     @staticmethod
-    def get_vectorizer(vectorizer_type: str, **kwargs) -> Union[TfidfVectorizer, CountVectorizer]:
+    def get_vectorizer(self: Any, vectorizer_type: str, **kwargs) -> Union[TfidfVectorizer, CountVectorizer]:
         """Retrieve the specified vectorizer class."""
-        vectorizers = {
-            "tfidf": TfidfVectorizer,
-            "bow": CountVectorizer,
-        }
         
-        vectorizer_cls = vectorizers.get(vectorizer_type.lower())
+        vectorizer_cls = self.vectorizers.get(vectorizer_type.lower())
         if not vectorizer_cls:
             raise ValueError(f"Unsupported vectorizer type: {vectorizer_type}")
         
