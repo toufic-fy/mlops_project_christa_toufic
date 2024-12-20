93c94993820af9acb5b6a49261c4da79f27b81d8
toufic-fy <160768965+toufic-fy@users.noreply.github.com>
2024-12-20 17:46:24 +0200

    Feature/t4.4 configuration add automation using py invoke (#16)
    * adding trainer class
    
    * adding invoke tasks
    
    * fixing linting issues in download script
    
    * updating gitignore to ignore docs
    
    ---------
    
    Co-authored-by: toufic.f <t.fakhry@ob-soft.com>

---
+++ tasks.py
@@ -0,0 +1,29 @@
+from invoke.context import Context
+from invoke.tasks import task
+
+@task
+def test(ctx: Context) -> None:
+    """Run all tests with pytest."""
+    ctx.run("poetry run pytest tests/")
+
+@task
+def lint(ctx: Context) -> None:
+    """Run ruff to lint and format the code."""
+    ctx.run("ruff check .")
+
+@task
+def format(ctx: Context) -> None:
+    """Run ruff to format the code."""
+    ctx.run("ruff check . --fix")
+
+@task
+def type(ctx: Context) -> None:
+    """Check the types with mypy."""
+    ctx.run("mypy src")
+
+@task
+def docs(ctx: Context) -> None:
+    """Generate HTML documentation with pdoc."""
+    ctx.run("pdoc email_classifier --output-dir docs")
+
+
